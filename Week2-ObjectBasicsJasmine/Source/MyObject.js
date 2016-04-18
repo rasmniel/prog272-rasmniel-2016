a4b2f0641aafac6c3cbeb95166fada8a820de01f
BCUSER <rasmniel@gmail.com>
2016-04-18 03:50:32 -0700

    beautify

--- Week2-ObjectBasicsJasmine/Source/MyObject.js
+++ Week2-ObjectBasicsJasmine/Source/MyObject.js
@@ -2,13 +2,13 @@ myObject = {
     num1: 1,
     num2: 2,
     string: 'This is a string property',
-    func1: function () {
+    func1: function() {
         return 'a number';
     },
-    func2: function () {
+    func2: function() {
         return 123;
     },
-    func3: function (a, b) {
+    func3: function(a, b) {
         return a + b;
     }
 };
