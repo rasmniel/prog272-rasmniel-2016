679fcc8d1e2524b38cc83326b4672969dded5c51
Rasmus Nielsen <rasmniel@gmail.com>
2016-05-27 04:39:26 +0000

    Refactored SolarVoyager

--- SolarVoyager/public/javascripts/control.js
+++ SolarVoyager/public/javascripts/control.js
@@ -7,32 +7,6 @@ define(['jquery', 'home', 'about', 'renewables', 'renewableByYear', 'renewableBy
             $('#display2').html('bar');
         }
 
-        /*function showRenewable(renewable) {
-            console.log('showing renewables!');
-            renewable = getSimpleKeys(renewable);
-            $('#yearView').val(renewable.year);
-            $('#solarView').val(renewable.solar);
-            $('#geoView').val(renewable.geo);
-            $('#otherBiomassView').val(renewable.otherBiomass);
-            $('#windView').val(renewable.wind);
-            $('#liquidBiofuelsView').val(renewable.liquidBiofuels);
-            $('#woodView').val(renewable.wood);
-            $('#hydropowerView').val(renewable.hydropower);
-        }
-
-        function getSimpleKeys(renewable) {
-            return {
-                year: renewable['Year'],
-                solar: renewable['Solar (quadrillion Btu)'],
-                geo: renewable['Geothermal (quadrillion Btu)'],
-                otherBiomass: renewable['Other biomass (quadrillion Btu)'],
-                wind: renewable['Wind power (quadrillion Btu)'],
-                liquidBiofuels: renewable['Liquid biofuels (quadrillion Btu)'],
-                wood: renewable['Wood biomass (quadrillion Btu)'],
-                hydropower: renewable['Hydropower (quadrillion Btu)']
-            };
-        }*/
-
         var control = {
             color: "black",
             size: "unisize",
